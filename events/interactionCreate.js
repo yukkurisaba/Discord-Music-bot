b9f8bb762d8f4f25ce8ebc8e529eb3304f89e275
yukkurisaba <99885166+yukkurisaba@users.noreply.github.com>
2022-04-16 23:54:50 +0900

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -8,9 +8,9 @@ module.exports = (client, int) => {
           if (!queue || !queue.playing) return int.reply({ content: `現在再生している音楽はありません ❌`, ephemeral: true, components: [] });
 
             int.member.send(`**Track Saved: \`${queue.current.title}\` | Posted by \`${queue.current.author}\`, Saved Server: \`${int.member.guild.name}\` ✅**`).then(() => {
-                return int.reply({ content: `I sent you the name of the music in a private message ✅`, ephemeral: true, components: [] });
+                return int.reply({ content: `DMに音楽のタイトルを送りました。 ✅`, ephemeral: true, components: [] });
             }).catch(error => {
-                return int.reply({ content: `I can't send you a private message. ❌`, ephemeral: true, components: [] });
+                return int.reply({ content: `DMに音楽のタイトルを送れませんでした。 ❌`, ephemeral: true, components: [] });
             });
         }
     }
