de6d23a70be6036736c4c4b9238152822ded7517
yukkurisaba <99885166+yukkurisaba@users.noreply.github.com>
2022-08-19 15:49:26 +0900

    Update loop.js

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -1,51 +1,29 @@
 const { QueueRepeatMode } = require('discord-player');
-const { ApplicationCommandOptionType } = require('discord.js');
 
 module.exports = {
     name: 'loop',
-    description: 'enable or disable looping of song\'s or the whole queue',
+    aliases: ['lp'],
+    utilisation: '{prefix}loop <queue>',
     voiceChannel: true,
-    options: [
-        {
-        name: 'action' ,
-        description: 'what action you want to preform on the loop',
-        type: ApplicationCommandOptionType.String,
-        required: true,
-        choices: [
-            { name: 'Queue', value: 'enable_loop_queue' },
-            { name: 'Disable', value: 'disable_loop'},
-            { name: 'Song', value: 'enable_loop_song' },
-        ],
-    }
-    ],
-    execute({ inter }) {
-        const queue = player.getQueue(inter.guildId);
 
-        if (!queue || !queue.playing) return inter.reply({ content: `No music currently playing ${inter.member}... try again ? ❌`, ephemeral: true });
-        switch (inter.options._hoistedOptions.map(x => x.value).toString()) {
-            case 'enable_loop_queue': {
-                if (queue.repeatMode === 1) return inter.reply({ content:`You must first disable the current music in the loop mode (/loop Disable) ${inter.member}... try again ? ❌`, ephemeral: true });
+    execute(client, message, args) {
+        const queue = client.player.getQueue(message.guild.id);
 
-                const success = queue.setRepeatMode( QueueRepeatMode.QUEUE);
+ 
+if (!queue || !queue.playing) return message.channel.send(`${message.author}, 現在再生中の音楽はありません ❌`);
 
-                return inter.reply({ content:success ? `Repeat mode **enabled** the whole queue will be repeated endlessly 🔁` : `Something went wrong ${inter.member}... try again ? ❌` });
-                break
-            }
-            case 'disable_loop': {
-                const success = queue.setRepeatMode(QueueRepeatMode.OFF);
+        if (args.join('').toLowerCase() === 'queue') {
+            if (queue.repeatMode === 1) return message.channel.send(`${message.author}, 既存のループを無効にする必要があります。 **(${client.config.px}loop)** ❌`);
 
-                return inter.reply({ content:success ? `Repeat mode **disabled**` : `Something went wrong ${inter.member}... try again ? ❌` });
-                break
-            }
-            case 'enable_loop_song': {
-                if (queue.repeatMode === 2) return inter.reply({ content:`You must first disable the current music in the loop mode (/loop Disable) ${inter.member}... try again ? ❌`, ephemeral: true });
+            const success = queue.setRepeatMode(queue.repeatMode === 0 ? QueueRepeatMode.QUEUE : QueueRepeatMode.OFF);
 
-                const success = queue.setRepeatMode( QueueRepeatMode.TRACK);
-                
-                return inter.reply({ content:success ? `Repeat mode **enabled** the current song will be repeated endlessly (you can end the loop with /loop disable)` : `Something went wrong ${inter.member}... try again ? ❌` });
-                break
-            }
-        }
-       
+            return message.channel.send(success ? `Loop Mode: **${queue.repeatMode === 0 ? '無効' : '有効'}**,再生中の音楽を繰り返します 🔁` : `${message.author}, どこかで間違っています ❌`);
+        } else {
+            if (queue.repeatMode === 2) return message.channel.send(`${message.author}, 既存のループを無効にする必要があります。 **(${client.config.px}loop queue)** ❌`);
+
+            const success = queue.setRepeatMode(queue.repeatMode === 0 ? QueueRepeatMode.TRACK : QueueRepeatMode.OFF);
+
+            return message.channel.send(success ? `Loop Mode: **${queue.repeatMode === 0 ? '無効' : '有効'}** (**${client.config.px}loop queue**で再生リストにあるすべての音楽を繰り返すことができます) 🔂` : `${message.author}, どこかで間違っています ❌`);
+};
     },
 };
