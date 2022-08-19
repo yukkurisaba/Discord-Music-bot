89cfbda503a9a79ede7f0e1320475ea3177aba17
yukkurisaba <99885166+yukkurisaba@users.noreply.github.com>
2022-08-19 09:58:21 +0900

    Update back.js

--- commands/music/back.js
+++ commands/music/back.js
@@ -1,18 +1,17 @@
 module.exports = {
     name: 'back',
-    aliases: [],
-    utilisation: '{prefix}back',
+    description: "Go back the song before",
     voiceChannel: true,
 
-    async execute(client, message) {
-        const queue = client.player.getQueue(message.guild.id);
+    async execute({ inter }) {
+        const queue = player.getQueue(inter.guildId);
 
-        if (!queue || !queue.playing) return message.channel.send(`${message.author}, 現在再生中の音楽はありません ❌`);
+        if (!queue || !queue.playing) return inter.reply({ content: `No music currently playing ${inter.member}... try again ? ❌`, ephemeral: true });
 
-        if (!queue.previousTracks[1]) return message.channel.send(`${message.author}, 以前再生していた音楽はありません ❌`);
+        if (!queue.previousTracks[1]) return inter.reply({ content: `There was no music played before ${inter.member}... try again ? ❌`, ephemeral: true });
 
         await queue.back();
 
-        message.channel.send(`ひとつ前の音楽を再生します... ✅`);
+        inter.reply({ content:`Playing the **previous** track ✅`});
     },
 };
