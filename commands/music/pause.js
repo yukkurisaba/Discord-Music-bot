ef2ee0dcd947c1fba483707e06cbfb706c955e7d
yukkurisaba <99885166+yukkurisaba@users.noreply.github.com>
2022-08-19 15:49:58 +0900

    Update pause.js

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -1,19 +1,16 @@
 module.exports = {
     name: 'pause',
-    description: 'pause the track',
+    aliases: [],
+    utilisation: '{prefix}pause',
     voiceChannel: true,
 
-    execute({ inter }) {
-        const queue = player.getQueue(inter.guildId);
+    execute(client, message) {
+        const queue = client.player.getQueue(message.guild.id);
 
-        if (!queue) return inter.reply({ content: `No music currently playing ${inter.member}... try again ? ❌`, ephemeral: true });
-        
-        if(queue.connection.paused) return inter.reply({content: 'The track is currently paused!', ephemeral: true})
-
-        if(queue.connection.paused) return inter.reply({content: `The track is currently paused, ${inter.member}... try again ? ❌`, ephemeral: true})
+       if (!queue || !queue.playing) return message.channel.send(`${message.author}, 現在再生中の音楽はありません ❌`);
 
         const success = queue.setPaused(true);
-        
-        return inter.reply({ content: success ? `Current music ${queue.current.title} paused ✅` : `Something went wrong ${inter.member}... try again ? ❌` });
+
+        return message.channel.send(success ? `**${queue.current.title}** をストップしました ✅` : `${message.author}, どこかで間違っています ❌`);
     },
 };
