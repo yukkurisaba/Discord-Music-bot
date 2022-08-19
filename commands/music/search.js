89805cf7148659fd0842a24f365e44c75d82df04
yukkurisaba <99885166+yukkurisaba@users.noreply.github.com>
2022-08-19 10:08:44 +0900

    Update search.js

--- commands/music/search.js
+++ commands/music/search.js
@@ -1,72 +1,75 @@
-const { MessageEmbed } = require('discord.js');
+const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
 const { QueryType } = require('discord-player');
 
 module.exports = {
     name: 'search',
-    aliases: [],
-    utilisation: '{prefix}search [song name]',
+    description: 'search a track',
     voiceChannel: true,
+    options: [
+        {
+            name: 'song',
+            description: 'the song you want to search',
+            type: ApplicationCommandOptionType.String,
+            required: true,
+        }
+    ],
 
-    async execute(client, message, args) {
-      
-if (!args[0]) return message.channel.send(`${message.author}, Please enter a valid song name. ❌`);
+    async execute({ client, inter }) {
+        const song = inter.options.getString('song');
 
-        const res = await client.player.search(args.join(' '), {
-            requestedBy: message.member,
+        const res = await player.search(song, {
+            requestedBy: inter.member,
             searchEngine: QueryType.AUTO
         });
 
-        if (!res || !res.tracks.length) return message.channel.send(`${message.author}, No search results found. ❌`);
+        if (!res || !res.tracks.length) return inter.reply({ content: `No results found ${inter.member}... try again ? ❌`, ephemeral: true });
 
-        const queue = await client.player.createQueue(message.guild, {
-            metadata: message.channel
+        const queue = await player.createQueue(inter.guild, {
+            metadata: inter.channel,
+            leaveOnEnd: client.config.opt.leaveOnEnd,
         });
-
-        const embed = new MessageEmbed();
-
-        embed.setColor('RED');
-        embed.setTitle(`Searched Music: ${args.join(' ')}`);
-
         const maxTracks = res.tracks.slice(0, 10);
 
-        embed.setDescription(`${maxTracks.map((track, i) => `**${i + 1}**. ${track.title} | ${track.author}`).join('\n')}\n\nChoose a song from **1** to **${maxTracks.length}** write send or write **cancel** and cancel selection.⬇️`);
+        const embed = new EmbedBuilder()
+        .setColor('#ff0000')
+        .setAuthor({ name: `Results for ${song}`, iconURL: client.user.displayAvatarURL({ size: 1024, dynamic: true })})
+        .setDescription(`${maxTracks.map((track, i) => `**${i + 1}**. ${track.title} | ${track.author}`).join('\n')}\n\nSelect choice between **1** and **${maxTracks.length}** or **cancel** ⬇️`)
+        .setTimestamp()
+        .setFooter({ text: 'Music comes first - Made with heart by Zerio ❤️', iconURL: inter.member.avatarURL({ dynamic: true })})
 
-        embed.setTimestamp();
-        embed.setFooter('Edited by Umut Bayraktar ❤️', message.author.avatarURL({ dynamic: true }));
+        inter.reply({ embeds: [embed] });
 
-        message.channel.send({ embeds: [embed] });
-
-        const collector = message.channel.createMessageCollector({
+        const collector = inter.channel.createMessageCollector({
             time: 15000,
+            max: 1,
             errors: ['time'],
-            filter: m => m.author.id === message.author.id
+            filter: m => m.author.id === inter.member.id
         });
 
-       collector.on('collect', async (query) => {
-            if (query.content.toLowerCase() === 'cancel') return message.channel.send(`Call cancelled. ✅`) && collector.stop();
+        collector.on('collect', async (query) => {
+            if (query.content.toLowerCase() === 'cancel') return inter.followUp({ content: `Search cancelled ✅`, ephemeral: true }), collector.stop();
 
-            const value = parseInt(query.content);
-
-            if (!value || value <= 0 || value > maxTracks.length) return message.channel.send(`Error: select a song **1** to **${maxTracks.length}** and write send or type **cancel** and cancel selection. ❌`);
+            const value = parseInt(query);
+            if (!value || value <= 0 || value > maxTracks.length) return inter.followUp({ content: `Invalid response, try a value between **1** and **${maxTracks.length}** or **cancel**... try again ? ❌`, ephemeral: true });
 
             collector.stop();
 
             try {
-                if (!queue.connection) await queue.connect(message.member.voice.channel);
+                if (!queue.connection) await queue.connect(inter.member.voice.channel);
             } catch {
-                await client.player.deleteQueue(message.guild.id);
-                return message.channel.send(`${message.author}, I can't join audio channel. ❌`);
+                await player.deleteQueue(inter.guildId);
+                return inter.followUp({ content: `I can't join the voice channel ${inter.member}... try again ? ❌`, ephemeral: true });
             }
 
-            await message.channel.send(`Loading your music call. 🎧`);
+            await inter.followUp(`Loading your search... 🎧`);
 
-            queue.addTrack(res.tracks[Number(query.content)-1]);
+            queue.addTrack(res.tracks[query.content - 1]);
+
             if (!queue.playing) await queue.play();
-           
         });
 
         collector.on('end', (msg, reason) => {
-            if (reason === 'time') return message.channel.send(`${message.author}, Song search time expired ❌`);
+            if (reason === 'time') return inter.followUp({ content:`Search timed out ${inter.member}... try again ? ❌`, ephemeral: true })
         });
     },
 };
