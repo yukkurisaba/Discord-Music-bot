4cbe6a01d14a26e69cb9e81cf9fba3cf5e921cce
yukkurisaba <99885166+yukkurisaba@users.noreply.github.com>
2022-08-19 10:06:04 +0900

    Update queue.js

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,36 +1,33 @@
-const { MessageEmbed } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     name: 'queue',
-    aliases: ['q'],
-    utilisation: '{prefix}queue',
+    description: 'Get the songs in the queue',
     voiceChannel: true,
 
-    execute(client, message) {
-        const queue = client.player.getQueue(message.guild.id);
+    execute({ client, inter }) {
+        const queue = player.getQueue(inter.guildId);
 
- 
-        if (!queue || !queue.playing) return message.channel.send(`${message.author}, There is no music currently playing!. ❌`);
+        if (!queue) return inter.reply({ content: `No music currently playing ${inter.member}... try again ? ❌`, ephemeral: true });
 
-        if (!queue.tracks[0]) return message.channel.send(`${message.author}, No music in queue after current. ❌`);
+        if (!queue.tracks[0]) return  inter.reply({ content: `No music in the queue after the current one ${inter.member}... try again ? ❌`, ephemeral: true });
 
-        const embed = new MessageEmbed();
-        const methods = ['🔁', '🔂'];
-
-        embed.setColor('RED');
-        embed.setThumbnail(message.guild.iconURL({ size: 2048, dynamic: true }));
-        embed.setTitle(`Server Music List - ${message.guild.name} ${methods[queue.repeatMode]}`);
-
-        const tracks = queue.tracks.map((track, i) => `**${i + 1}** - ${track.title} | ${track.author} (Started by <@${track. requestedBy.id}>)`);
+        const methods = ['', '🔁', '🔂'];
 
         const songs = queue.tracks.length;
-        const nextSongs = songs > 5 ? `And **${songs - 5}** Other Song...` : `There are **${songs}** Songs in the List.`;
 
-        embed.setDescription(`Currently Playing: \`${queue.current.title}\`\n\n${tracks.slice(0, 5).join('\n')}\n\n${nextSongs }`);
+        const nextSongs = songs > 5 ? `And **${songs - 5}** other song(s)...` : `In the playlist **${songs}** song(s)...`;
 
-        embed.setTimestamp();
-        embed.setFooter('Remixed by いるか', message.author.avatarURL({ dynamic: true }));
+        const tracks = queue.tracks.map((track, i) => `**${i + 1}** - ${track.title} | ${track.author} (requested by : ${track.requestedBy.username})`)
 
-        message.channel.send({ embeds: [embed] });
+        const embed = new EmbedBuilder()
+        .setColor('#ff0000')
+        .setThumbnail(inter.guild.iconURL({ size: 2048, dynamic: true }))
+        .setAuthor({name: `Server queue - ${inter.guild.name} ${methods[queue.repeatMode]}`, iconURL: client.user.displayAvatarURL({ size: 1024, dynamic: true })})
+        .setDescription(`Current ${queue.current.title}\n\n${tracks.slice(0, 5).join('\n')}\n\n${nextSongs}`)
+        .setTimestamp()
+        .setFooter({ text: 'Music comes first - Made with heart by Zerio ❤️', iconURL: inter.member.avatarURL({ dynamic: true })})
+
+        inter.reply({ embeds: [embed] });
     },
 };
