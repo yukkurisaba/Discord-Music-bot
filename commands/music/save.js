44c6b0ae562ddeed4ae7259e7a0285d56fed4872
yukkurisaba <99885166+yukkurisaba@users.noreply.github.com>
2022-08-19 15:51:49 +0900

    Update save.js

--- commands/music/save.js
+++ commands/music/save.js
@@ -1,34 +1,18 @@
-const { EmbedBuilder } = require("discord.js");
-
 module.exports = {
     name: 'save',
-    description: 'save the current track!',
+    aliases: [],
+    utilisation: '{prefix}save',
     voiceChannel: true,
 
-    async execute({ inter }) {
-        const queue = player.getQueue(inter.guildId);
+    async execute(client, message) {
+        const queue = client.player.getQueue(message.guild.id);
 
-        if (!queue) return inter.reply({ content: `No music currently playing ${inter.member}... try again ? ❌`, ephemeral: true });
+  if (!queue || !queue.playing) return message.channel.send(`${message.author}, 現在再生中の音楽はありません ❌`);
 
-        inter.member.send({
-            embeds: [
-                new EmbedBuilder()
-                    .setColor('Red')
-                    .setTitle(`:arrow_forward: ${queue.current.title}`)
-                    .setURL(queue.current.url)
-                    .addFields(
-                        { name: ':hourglass: Duration:', value: `\`${queue.current.duration}\``, inline: true },
-                        { name: 'Song by:', value: `\`${queue.current.author}\``, inline: true },
-                        { name: 'Views :eyes:', value: `\`${Number(queue.current.views).toLocaleString()}\``, inline: true },
-                        { name: 'Song URL:', value: `\`${queue.current.url}\`` }
-                    )
-                    .setThumbnail(queue.current.thumbnail)
-                    .setFooter({text:`from the server ${inter.member.guild.name}`, iconURL: inter.member.guild.iconURL({ dynamic: false })})
-            ]
-        }).then(() => {
-            return inter.reply({ content: `I have sent you the title of the music by private messages ✅`, ephemeral: true });
+        message.author.send(`登録曲: **${queue.current.title}** | ${queue.current.author}, __Saved server: **${message.guild.name}**__ ✅`) .then(() => {
+            message.channel.send(`プライベートメッセージに音楽名を送りました ✅`);
         }).catch(error => {
-            return inter.reply({ content: `Unable to send you a private message... try again ? ❌`, ephemeral: true });
+            message.channel.send(`${message.author}, プライベートメッセージを送信できませんでした ❌`);
         });
     },
 };
