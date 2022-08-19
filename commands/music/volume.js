6d86b1e5bd484dcd64d92319b9140dc64d6cdf72
yukkurisaba <99885166+yukkurisaba@users.noreply.github.com>
2022-08-19 15:53:02 +0900

    Update volume.js

--- commands/music/volume.js
+++ commands/music/volume.js
@@ -1,31 +1,26 @@
-const maxVol = client.config.opt.maxVol;
-const { ApplicationCommandOptionType } = require('discord.js');
+const maxVol = require("../../config.js").opt.maxVol;
 
 module.exports = {
     name: 'volume',
-    description: 'adjust',
+    aliases: ['vol'],
+    utilisation: `{prefix}volume [1-${maxVol}]`,
     voiceChannel: true,
-    options: [
-        {
-            name: 'volume',
-            description: 'the amount volume',
-            type: ApplicationCommandOptionType.Number,
-            required: true,
-            minValue: 1,
-            maxValue: maxVol
-        }
-    ],
 
-    execute({ inter }) {
-        const queue = player.getQueue(inter.guildId);
+    execute(client, message, args) {
+        const queue = client.player.getQueue(message.guild.id);
 
-        if (!queue) return inter.reply({ content: `No music currently playing ${inter.member}... try again ? ❌`, ephemeral: true });
-        const vol = inter.options.getNumber('volume')
+       if (!queue || !queue.playing) return message.channel.send(`${message.author}, 現在再生中の音楽はありません ❌`);
 
-        if (queue.volume === vol) return inter.reply({ content: `The volume you want to change is already the current one ${inter.member}... try again ? ❌`, ephemeral: true });
+        const vol = parseInt(args[0]);
+
+        if (!vol) return message.channel.send(`現在の音量: **${queue.volume}** 🔊\n**ボリュームを変更するには \`1\` ~ \`${maxVol}\` の数字を入力してください**`);
+
+        if (queue.volume === vol) return message.channel.send(`${message.author}, 変更しようとした音量は、すでに現在の音量です ❌`);
+
+        if (vol < 0 || vol > maxVol) return message.channel.send(`${message.author}, **ボリュームを変更する場合は、\`1\` ~ \`${maxVol}\` の数字を入力してください** ❌`);
 
         const success = queue.setVolume(vol);
 
-        return inter.reply({ content:success ? `The volume has been modified to **${vol}**/**${maxVol}**% 🔊` : `Something went wrong ${inter.member}... try again ? ❌`});
+        return message.channel.send(success ? `音量変更: **${vol}**/**${maxVol}%** 🔊` : `${message.author}, どこかで間違っています ❌`) ;
     },
 };
