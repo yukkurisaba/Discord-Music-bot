b01b770ecb8240b00367fbd7cec7bdc75cee2612
yukkurisaba <99885166+yukkurisaba@users.noreply.github.com>
2022-08-19 15:45:13 +0900

    Delete src/buttons directory

--- src/buttons/skip.js
+++
@@ -1,7 +0,0 @@
-module.exports = async ({  inter, queue }) => { 
-    if (!queue || !queue.playing) return inter.reply({ content: `No music currently playing... try again ? ❌`, ephemeral: true });
-    
-    const success = queue.skip();
-
-    return inter.reply({ content: success ? `Current music ${queue.current.title} skipped ✅` : `Something went wrong ${inter.member}... try again ? ❌`, ephemeral: true});
-}
